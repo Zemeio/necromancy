ec79b025d309f4e2e6b9392173845b4183def7f4
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-03-06 08:33:51 +0900

    Add tiebreakers and finalize turn order for one action

--- Scenes/Board/TurnOrder.gd
+++ Scenes/Board/TurnOrder.gd
@@ -11,6 +11,12 @@ var _action_reference = {}
 var _unit_attributes := {}
 
 
+func _init():
+	randomize()
+
+
+const IS_DEBUG := true
+
 func _add_action(action: TurnAction):
 	if typeof(action) != typeof(TurnAction):
 		print("Action is not of type TurnAction!")
@@ -37,12 +43,12 @@ func __calculate_random_initial_wait():
 	return (randi() % 50) + 51
 
 func initialize(units):
-	_calculate_turn_order(units)
-	for unit in units:
+	var unit_turns = _calculate_turn_order(units)
+	for unit in unit_turns:
 		addCharacterTurn(unit)
 
 
-func _calculate_turn_order(units: Array):
+func _calculate_turn_order(units: Array) -> Array:
 	_initialize_non_initialized_units(units)
 
 	var remaining_units := units.duplicate(false)
@@ -80,28 +86,25 @@ func _find_units_that_can_act_now(waits_per_speed):
 			units_that_have_0_or_less_wait.append(unit)
 	return units_that_have_0_or_less_wait
 
-func _resolve_tiebreaker1():
-	pass
-
-func _resolve_tiebreaker2():
-	pass
-
 func _resolve_tiebreakers(units_that_can_act: Array, waits_per_speed: Dictionary):
 	if units_that_can_act.size() == 1: return units_that_can_act # No ties
 
-	var final_order = []
-	var groups = _resolve_tiebreaker1()
-	for group in groups:
-		if group.size() > 1:
-			group = _resolve_tiebreaker2()
-		final_order.append_array(group)
+	var tiebreakers = TiebreakerWait.new()\
+						.otherwise(TiebreakerSpeed.new())\
+						.otherwise(TiebreakerFinal.new())
+
+	var units_order_by_tiebreak: Array = tiebreakers.resolve(units_that_can_act, waits_per_speed)
+	return units_order_by_tiebreak
 
 func _initialize_non_initialized_units(units):
-
 	for unit in units:
 		if not unit in _unit_attributes:
 			_unit_attributes[unit] = {
 					wait = __calculate_random_initial_wait()
+				}
+			if IS_DEBUG:
+				_unit_attributes[unit] = {
+					wait = unit.UnitInfo._debug_wait
 				}
 
 func _update_ui():
@@ -109,3 +112,68 @@ func _update_ui():
 	for action in actionList:
 		if action.should_show_in_ui:
 			itemListWindow.add_item(action.name)
+
+class Tiebreaker:
+	var next_method: Tiebreaker = null
+	
+	func set_last(next_method: Tiebreaker):
+		if self.next_method == null:
+			self.next_method = next_method
+		else:
+			self.next_method.otherwise(next_method)
+	
+	func otherwise(next_method: Tiebreaker) -> Tiebreaker:
+		self.set_last(next_method)
+		return self
+
+	func resolve(group, waits_per_speed):
+		if group.size() == 1: return group # Nothing to resolve
+		var result_groups: Array = self.__resolve_group(group, waits_per_speed)
+
+		var final_result := []
+		for resolved_group in result_groups:
+			if resolved_group.size() > 1 and next_method != null:
+				resolved_group = self.next_method.resolve(resolved_group, waits_per_speed) # needs further resolution
+			final_result.append_array(resolved_group)
+		return final_result
+
+	func __resolve_group(group, waits_per_speed):
+		var grouped_per_waits: Dictionary = self._group_by(group, waits_per_speed)
+		var resolved_groups := []
+		var keys: Array = grouped_per_waits.keys()
+		keys.sort()
+		for wait in keys:
+			resolved_groups.append(grouped_per_waits[wait])
+		return resolved_groups
+		
+	func _group_by(group, waits_per_speed) -> Dictionary:
+		# Abstract method
+		return {}
+
+class TiebreakerWait extends Tiebreaker:
+
+	func _group_by(group, waits_per_speed) -> Dictionary:
+		var grouped_per_waits := {}
+		for unit in group:
+			var unit_wait = waits_per_speed[unit]
+			if not unit_wait in grouped_per_waits:
+				grouped_per_waits[unit_wait] = []
+			grouped_per_waits[unit_wait].append(unit)
+		return grouped_per_waits
+
+
+class TiebreakerSpeed extends Tiebreaker:
+
+	func _group_by(group, waits_per_speed):
+		var grouped_per_speed := {}
+		for unit in group:
+			var unit_speed = -unit.get_speed() # Uses negative because priority is from top to bottom
+			if not unit_speed in grouped_per_speed:
+				grouped_per_speed[unit_speed] = [] 
+			grouped_per_speed[unit_speed].append(unit)
+		return grouped_per_speed
+
+class TiebreakerFinal extends Tiebreaker:
+	
+	func resolve(group, waits_per_speed):
+		return group
